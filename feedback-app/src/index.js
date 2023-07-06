afbb4f86e920495ac704fc60c4e7813c95bc4114
Spidey44 <83247073+Spidey44@users.noreply.github.com>
2023-07-06 10:09:17 -0400

    test1009

--- feedback-app/src/index.js
+++ feedback-app/src/index.js
@@ -2,7 +2,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-import App from './App'
+//import App from './App'
 
 // interact with ReactDom object model
 // render takes 2. #1 main the app component to be inserted. #2 the rood id
