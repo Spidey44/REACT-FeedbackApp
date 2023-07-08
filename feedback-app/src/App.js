ac886abfba9b023be7c2c6be1349475baff63d2b
Spidey44 <83247073+Spidey44@users.noreply.github.com>
2023-07-08 07:31:36 -0400

    Dynamic Values & Lists in JSX

--- feedback-app/src/App.js
+++ feedback-app/src/App.js
@@ -1,4 +1,21 @@
 function App() {
-    return <h1>HEllo from the app component</h1>
+    const title = 'Blog Post'
+    const body = 'This is my blog post'
+    const comments = [
+        { id: 1, text: 'comment one' },
+        { id: 2, text: 'comment two' },
+        { id: 3, text: 'comment three' },
+    ]
+    return (<div ClassName='container'>
+        <h1>{title.toUpperCase()}</h1>
+        <p>{body}</p>
+        <div className="comments"></div>
+        <h3>Comments ({comments.length})</h3>
+        {/* ul created for each item unique (index). Use Map function */}
+        <ul>{comments.map((comment, index) =>
+            <li key={index}>{comment.text}</li>)
+        }
+        </ul>
+    </div>)
 }
 export default App
--- feedback-app/src/index.js
+++ feedback-app/src/index.js
@@ -3,7 +3,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import './index.css'
-//import App from './App'
 
 // interact with ReactDom object model
 // render takes 2. #1 main the app component to be inserted2. #2 the rood id
