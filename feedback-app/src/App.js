6331bf884821808e3b65a7eb3142d8a7e2a2e6bb
Spidey44 <83247073+Spidey44@users.noreply.github.com>
2023-07-08 08:26:41 -0400

    Conditionals

--- feedback-app/src/App.js
+++ feedback-app/src/App.js
@@ -6,16 +6,30 @@ function App() {
         { id: 2, text: 'comment two' },
         { id: 3, text: 'comment three' },
     ]
-    return (<div ClassName='container'>
-        <h1>{title.toUpperCase()}</h1>
-        <p>{body}</p>
-        <div className="comments"></div>
-        <h3>Comments ({comments.length})</h3>
+    // use case: waiting API data to be fetched
+    const loading = false
+    const showComments = true
+
+    const JSXCommentBlock = (<div className="comments">
+        <h3> Comments({comments.length})</h3>
         {/* ul created for each item unique (index). Use Map function */}
         <ul>{comments.map((comment, index) =>
             <li key={index}>{comment.text}</li>)
         }
         </ul>
     </div>)
+
+    if (loading) return <h1>Loading....</h1>
+
+    return (<div ClassName='container'>
+        <h1>{title.toUpperCase()}</h1>
+        <p>{body}</p>
+
+        {/* option to show comment with ternary or &&  */}
+        {showComments ? 'yes' : 'no'}
+        {showComments && JSXCommentBlock}
+    </div >
+
+    )
 }
 export default App
