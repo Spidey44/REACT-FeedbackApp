d0476aa3dcf1686774bf45fa0cff62b67f370047
Spidey44 <83247073+Spidey44@users.noreply.github.com>
2023-07-07 22:22:33 -0400

    initializing react

---
+++ feedback-app/src/App.js
@@ -0,0 +1,4 @@
+function App() {
+    return <h1>HEllo from the app component</h1>
+}
+export default App
--- feedback-app/src/index.js
+++ feedback-app/src/index.js
@@ -1,14 +1,14 @@
 // idex.js = entry point to react
 import React from 'react'
 import ReactDOM from 'react-dom'
+import App from './App'
 import './index.css'
 //import App from './App'
 
 // interact with ReactDom object model
 // render takes 2. #1 main the app component to be inserted2. #2 the rood id
 ReactDOM.render(
-    <h1>
+    <React.StrictMode>
         <App />
-    </h1>,
-    document.getElementById('root')
-)
+    </React.StrictMode>,
+    document.getElementById('root'))
