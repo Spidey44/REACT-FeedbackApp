1de6d708b32a7af111b1edd720783a545605f7d6
Spidey44 <83247073+Spidey44@users.noreply.github.com>
2023-09-26 14:40:23 -0400

    navbar

--- marketplace/src/App.js
+++ marketplace/src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-
+import Navbar from './components/Navbar'
 import Explore from './pages/Explore'
 import Offers from './pages/Offers'
 import Profile from './pages/Profile'
@@ -21,7 +21,7 @@ function App() {
           <Route path='/Sign-up' element={<SignUp />} />
           <Route path='/Forgot-Password' element={<ForgotPassword />} />
         </Routes>
-
+        <Navbar />
       </Router>
     </>
   );
